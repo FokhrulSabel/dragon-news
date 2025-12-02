a1d75f3ac8203b198574c9c8cdbd4a3ab399367a
FokhrulSabel <fokhrulislam316@gmail.com>
2025-12-03 01:00:33 +0600

    3-theme customised + font added + date-fns integrated + header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
+import logo from "../assets/logo.png";
+import { format } from "date-fns";
 
 const Header = () => {
-    return (
-        <div>
-            <h1>header</h1>
-        </div>
-    );
+  return (
+    <div className="flex justify-center flex-col items-center gap-4">
+      <img className="w-[400px]" src={logo} alt="" />
+      <p className="text-accent">Journalism Without Fear or Favour</p>
+      <p className="text-semibold text-accent">
+        {format(new Date(), "EEEE , MMMM dd , yyyy")}
+      </p>
+    </div>
+  );
 };
 
 export default Header;
