ae825c4b391afc5e9b894215b1f1bd36cbeb7b18
FokhrulSabel <fokhrulislam316@gmail.com>
2025-12-04 00:19:41 +0600

    display categories & social Login UI

--- src/components/homeLayout/RighAside.jsx
+++ src/components/homeLayout/RighAside.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import SocialLogin from './SocialLogin';
 
 const RighAside = () => {
     return (
         <div>
-            right
+            <SocialLogin></SocialLogin>
         </div>
     );
 };
