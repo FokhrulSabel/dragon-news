95b7ce622539491c94e84191ec97fc200ba5f024
FokhrulSabel <fokhrulislam316@gmail.com>
2025-12-06 20:28:20 +0600

    hydrateFallbackElement and loading for better experience

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,36 +1,39 @@
-import React from 'react';
-import { Outlet } from 'react-router';
-import Header from '../components/Header';
-import LatestNews from '../components/LatestNews';
-import Navbar from '../components/Navbar';
-import LeftAside from '../components/homeLayout/LeftAside';
-import RighAside from '../components/homeLayout/RighAside';
+import React from "react";
+import { Outlet, useNavigate } from "react-router";
+import Header from "../components/Header";
+import LatestNews from "../components/LatestNews";
+import Navbar from "../components/Navbar";
+import LeftAside from "../components/homeLayout/LeftAside";
+import RighAside from "../components/homeLayout/RighAside";
+import Loading from "../pages/Loading";
 
 const HomeLayout = () => {
-    return (
-        <div>
-            <header>
-                <Header></Header>
-                <section className="w-11/12 mx-auto my-3">
-                    <LatestNews></LatestNews>
-                </section>
-                <nav className="w-11/12 mx-auto my-3">
-                    <Navbar></Navbar>
-                </nav>
-            </header>
-            <main className="w-11/12 mx-auto my-3  grid grid-cols-12 gap-5">
-                <aside className='col-span-3 sticky top-0 h-fit'>
-                    <LeftAside></LeftAside>
-                </aside>
-                <section className="main col-span-6">
-                    <Outlet></Outlet>
-                </section>
-                <aside className='col-span-3 sticky top-0 h-fit'>
-                    <RighAside></RighAside>
-                </aside>
-            </main>
-        </div>
-    );
+  const { state } = useNavigate();
+  return (
+    <div>
+      <header>
+        <Header></Header>
+        <section className="w-11/12 mx-auto my-3">
+          <LatestNews></LatestNews>
+        </section>
+        <nav className="w-11/12 mx-auto my-3">
+          <Navbar></Navbar>
+        </nav>
+      </header>
+      <main className="w-11/12 mx-auto my-3  grid grid-cols-12 gap-5">
+        <aside className="col-span-3 sticky top-0 h-fit">
+          <LeftAside></LeftAside>
+        </aside>
+        <section className="main col-span-6">
+            {state == "loading" ? <Loading/> : <Outlet></Outlet>}
+          
+        </section>
+        <aside className="col-span-3 sticky top-0 h-fit">
+          <RighAside></RighAside>
+        </aside>
+      </main>
+    </div>
+  );
 };
 
 export default HomeLayout;
